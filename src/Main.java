b6fd54255909e51b9d831ee44aad0582c0ea9f06
Biniyam-k <116093655+Biniyam-k@users.noreply.github.com>
2022-12-01 19:23:20 +0300

    Library

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Book Book1 = new Book();
+        Book1.bookName();
+        Book1.author();
+        Book1.dateBorrowed();
+        Book1.dateReturned();
+
+
+    }
+}
